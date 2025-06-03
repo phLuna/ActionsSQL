229234c5c3a7053347b75a639a4cc79208a015ef
Luna. <lunissnyan11@gmail.com>
2025-06-03 10:58:23 -0300

    Organização das rotas das metas também.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
-from toolbox.functions import adicionar_meta, comparar_alocacao, deletar_meta
-
-from src.models.base import Base
-from src.models.inputs import MetaInput
 from src.integrations.sqlalchemy import SQLAlchemy
+from src.models.base import Base
 
 from src.rotes.acoes import router as acoes_router
+from src.rotes.metas import router as metas_router
 
 engine = SQLAlchemy.engine  # aqui apenas referencia o engine já criado
 Base.metadata.create_all(bind=engine)
@@ -16,28 +14,4 @@ app = FastAPI()
 
 #Método para incluir as rotas de src.rotes.acoes
 app.include_router(acoes_router)
-
-#Método para definir metas para as ações no DB.
-@app.post("/metas/")
-def definir_meta(entrada: MetaInput):
-    """Adiciona uma meta de quantia de tal ação na carteira."""
-
-    ticker = entrada.ticker
-    porcentagem = entrada.porcentagem
-
-    response = adicionar_meta(ticker, porcentagem)
-    return response
-
-#Método para excluir a meta de uma ação no DB.
-@app.delete("/meta-alocacao/{ticker}")
-def excluir_meta(ticker: str):
-    sucesso = deletar_meta(ticker)
-    if not sucesso:
-        raise HTTPException(status_code=404, detail="Meta de alocação não encontrada.")
-    return {"mensagem": f"Meta de alocação para '{ticker}' excluída com sucesso."}
-
-#Método para ver todas as metas e compará-las no DB.
-@app.get("/metas/")
-def comparar_metas():
-    response = comparar_alocacao()
-    return response
+app.include_router(metas_router)
---
+++ src/rotes/metas.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+
+from toolbox.functions import adicionar_meta, deletar_meta, comparar_alocacao
+
+from src.models.inputs import MetaInput
+
+router = APIRouter()
+
+#Método para definir metas para as ações no DB.
+@router.post("/metas/")
+def definir_meta(entrada: MetaInput):
+    """Adiciona uma meta de quantia de tal ação na carteira."""
+
+    ticker = entrada.ticker
+    porcentagem = entrada.porcentagem
+
+    response = adicionar_meta(ticker, porcentagem)
+    return response
+
+#Método para excluir a meta de uma ação no DB.
+@router.delete("/meta-alocacao/{ticker}")
+def excluir_meta(ticker: str):
+    sucesso = deletar_meta(ticker)
+    if not sucesso:
+        raise HTTPException(status_code=404, detail="Meta de alocação não encontrada.")
+    return {"mensagem": f"Meta de alocação para '{ticker}' excluída com sucesso."}
+
+#Método para ver todas as metas e compará-las no DB.
+@router.get("/metas/")
+def comparar_metas():
+    response = comparar_alocacao()
+    return response
